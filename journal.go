a4d2f79f033752234d3c341da44211afb5ab221f
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-03-24 16:11:30 -0400

    []byte

--- journal.go
+++ journal.go
@@ -4,31 +4,31 @@ package karigo
 type Journal struct{}
 
 // Append ...
-func (j *Journal) Append(c Entry) error {
+func (j *Journal) Append(c []byte) error {
 	return nil
 }
 
 // Get ...
-func (j *Journal) Get(id string) (Entry, error) {
-	return Entry{}, nil
+func (j *Journal) Get(id string) ([]byte, error) {
+	return []byte{}, nil
 }
 
 // Last ...
-func (j *Journal) Last() (Entry, error) {
-	return Entry{}, nil
+func (j *Journal) Last() ([]byte, error) {
+	return []byte{}, nil
 }
 
 // At ...
-func (j *Journal) At(v uint64) (Entry, error) {
-	return Entry{}, nil
+func (j *Journal) At(v uint64) ([]byte, error) {
+	return []byte{}, nil
 }
 
 // Tail ...
-func (j *Journal) Tail(n uint) ([]Entry, error) {
-	return []Entry{}, nil
+func (j *Journal) Tail(n uint) ([][]byte, error) {
+	return [][]byte{}, nil
 }
 
 // Range ...
-func (j *Journal) Range(n uint) ([]Entry, error) {
-	return []Entry{}, nil
+func (j *Journal) Range(n uint) ([][]byte, error) {
+	return [][]byte{}, nil
 }
