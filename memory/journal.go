4b48ec81e75f78d144ac026ec057e1c7323bfd2e
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-04-21 10:04:40 -0400

    Stuff I forgot

---
+++ memory/journal.go
@@ -0,0 +1,29 @@
+package memory
+
+// Journal ...
+type Journal struct{}
+
+// Append ...
+func (j *Journal) Append(c []byte) error {
+	return nil
+}
+
+// Last ...
+func (j *Journal) Last() (uint64, []byte, error) {
+	return 0, []byte{}, nil
+}
+
+// At ...
+func (j *Journal) At(i uint64) ([]byte, error) {
+	return []byte{}, nil
+}
+
+// Cut ...
+func (j *Journal) Cut(i uint64) error {
+	return nil
+}
+
+// Range ...
+func (j *Journal) Range(n uint, s uint) ([][]byte, error) {
+	return [][]byte{}, nil
+}
---
+++ memory/source_test.go
@@ -0,0 +1,15 @@
+package memory
+
+import (
+	"testing"
+
+	"github.com/mfcochauxlaberge/karigo/sourcetest"
+)
+
+func TestMemorySource(t *testing.T) {
+	src := &Source{}
+	err := sourcetest.Test(src)
+	if err != nil {
+		t.Errorf("Source %T failed: %s", src, err)
+	}
+}
