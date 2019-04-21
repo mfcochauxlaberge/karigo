b83d554753951a2f75dfc4926605b1d936a353a7
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-04-21 17:36:32 -0400

    Implement part of memory.Journal

--- memory/journal.go
+++ memory/journal.go
@@ -1,29 +1,55 @@
 package memory
 
+import "fmt"
+
 // Journal ...
-type Journal struct{}
+type Journal struct {
+	log   [][]byte
+	start uint
+}
 
 // Append ...
 func (j *Journal) Append(c []byte) error {
+	j.check()
+	j.log = append(j.log, c)
 	return nil
 }
 
 // Last ...
-func (j *Journal) Last() (uint64, []byte, error) {
-	return 0, []byte{}, nil
+func (j *Journal) Last() (uint, []byte, error) {
+	j.check()
+	if len(j.log) > 0 {
+		last := j.start + uint(len(j.log)) - 1
+		return last, j.log[len(j.log)-1], nil
+	}
+	return 0, nil, nil
 }
 
 // At ...
-func (j *Journal) At(i uint64) ([]byte, error) {
-	return []byte{}, nil
+func (j *Journal) At(i uint) ([]byte, error) {
+	j.check()
+	if i < j.start || i > j.start+uint(len(j.log))-1 {
+		return nil, fmt.Errorf("karigo: index %d does not exist", i)
+	}
+	return j.log[i-j.start], nil
 }
 
 // Cut ...
-func (j *Journal) Cut(i uint64) error {
+func (j *Journal) Cut(i uint) error {
+	j.check()
+	// TODO
 	return nil
 }
 
 // Range ...
 func (j *Journal) Range(n uint, s uint) ([][]byte, error) {
+	j.check()
+	// TODO
 	return [][]byte{}, nil
+}
+
+func (j *Journal) check() {
+	if j.log == nil {
+		j.log = [][]byte{}
+	}
 }
