5948a8845d4f1d90a532876d7ded54cfe84ebf31
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-06-29 18:05:41 -0400

    Save

--- cmd/karigo/commands/exec.go
+++ cmd/karigo/commands/exec.go
@@ -6,10 +6,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	commit *bool
+)
+
 var cmdExec = &cobra.Command{
 	Use:   "exec",
 	Short: "Execute an operation",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Operation committed.\n")
+		fmt.Printf("Operation is valid.\n")
+
+		if *commit {
+			fmt.Printf("Operation committed.\n")
+		}
 	},
+}
+
+func init() {
+	commit = cmdExec.Flags().BoolP("commit", "", false, "commit the operation")
 }
