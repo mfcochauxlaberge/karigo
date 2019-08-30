db6b0f9b74215f98a7e396b2be649fdcb10c77fb
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-08-29 21:40:36 -0400

    Stop respecting maximum line length in doc.go

--- doc.go
+++ doc.go
@@ -1,13 +1,6 @@
 /*
-Package karigo provides an HTTP API framework where the business logic is
-executed through transactions appended to an ordered log. The storage is seen
-as a list of keys and values and the transactions are simply a list of updates
-to apply to the keys.
+Package karigo provides an HTTP API framework where the business logic is executed through transactions appended to an ordered log. The storage is seen as a list of keys and values and the transactions are simply a list of updates to apply to the keys.
 
-An ordered log is an easy data structure to implement and mutate. In karigo,
-the log is meant to be exposed and handled directly. Components like search
-indexes and caches can simply read the log to stay up-to-date. The log can
-also be easily replicated through a consencus algorithm like Raft to improve
-avalaibility.
+An ordered log is an easy data structure to implement and mutate. In karigo, the log is meant to be exposed and handled directly. Components like search indexes and caches can simply read the log to stay up-to-date. The log can also be easily replicated through a consencus algorithm like Raft to improve avalaibility.
 */
 package karigo
