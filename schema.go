6116eb8e49b3716e80e80731ed1570b8ecebfea9
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-09-02 20:29:05 -0400

    Add commented code

--- schema.go
+++ schema.go
@@ -133,4 +133,153 @@ type op struct {
 	Version string `json:"version" api:"rel,0_log,ops"`
 }
 
-func handleSchemaChange(r *Request, cp *Checkpoint, s *jsonapi.Schema) {}
+func handleSchemaChange(r *Request, cp *Checkpoint, s *jsonapi.Schema) {
+	// var (
+	// 	res jsonapi.Resource
+	// 	ops []Op
+	// 	err error
+	// )
+
+	// res, _ = r.Body.Data.(jsonapi.Resource)
+
+	// err = validateSchemaChange(res)
+	// if err != nil {
+	// 	cp.Check(err)
+	// }
+
+	// if r.Method == "POST" {
+	// 	if res.GetType().Name == "0_sets" {
+	// 		// Add set
+	// 		ops, err = addSet(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	} else if res.GetType().Name == "0_attrs" {
+	// 		// Add attribute
+	// 		ops, err = addAttr(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	} else if res.GetType().Name == "0_rels" {
+	// 		// Add relationship
+	// 		ops, err = addRel(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	}
+
+	// } else if r.Method == "PATCH" {
+	// 	// Can only be for activating or deactivating
+	// 	// a set, attribute, or relationship.
+	// 	if activate, ok := res.Get("active").(bool); activate && ok {
+	// 		ops, err = activateSet(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	}
+
+	// 	if deactivate, ok := res.Get("active").(bool); !deactivate && ok {
+	// 		ops, err = deactivateSet(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	}
+	// } else if r.Method == "DELETE" {
+	// 	currRes := cp.Resource(QueryRes{
+	// 		Set:    res.GetType().Name,
+	// 		ID:     res.GetID(),
+	// 		Fields: []string{"active"},
+	// 	})
+
+	// 	if currRes.GetID() == "" {
+	// 		cp.Fail(errors.New("schema element does not exist"))
+	// 	}
+
+	// 	if active, _ := currRes.Get("active").(bool); !active {
+	// 		// Only possible is active is false
+	// 		ops, err = deleteSet(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	} else {
+	// 		cp.Fail(errors.New("schema element is still active"))
+	// 	}
+	// }
+}
+
+// func validateSchemaChange(res jsonapi.Resource) error {
+// 	if res.Get("name") == "" {
+// 		return fmt.Errorf("karigo: %q is not a valid JSON:API name", res.Get("name"))
+// 	}
+// 	return nil
+// }
+
+// func addSet(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	name := res.Get("name").(string)
+
+// 	current := s.GetType(name)
+// 	if current.Name != "" {
+// 		return nil, fmt.Errorf("type name %q already exists", name)
+// 	}
+
+// 	ops := []Op{NewOpAdd("0_sets", "", "id", name)}
+// 	return ops, nil
+// }
+
+// func deleteSet(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	s.RemoveType(res.GetID())
+
+// 	ops := []Op{
+// 		NewOpSet(
+// 			res.GetType().Name,
+// 			res.GetID(),
+// 			"id",
+// 			"",
+// 		),
+// 	}
+
+// 	return ops, nil
+// }
+
+// func activateSet(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	typ := jsonapi.Type{
+// 		Name: res.Get("name").(string),
+// 	}
+
+// 	err := s.AddType(typ)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return []Op{}, nil
+// }
+
+// func deactivateSet(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
+
+// func addAttr(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
+
+// func deleteAttr(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
+
+// func activateAttr(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
+
+// func deactivateAttr(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
+
+// func addRel(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
+
+// func deleteRel(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
+
+// func activateRel(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
+
+// func deactivateRel(s *jsonapi.Schema, res jsonapi.Resource) ([]Op, error) {
+// 	return []Op{}, nil
+// }
