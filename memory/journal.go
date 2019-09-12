ee5da673b49aefa2ee9b103aeaff03089c59a7d2
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-09-11 23:00:57 -0400

    Save

--- memory/journal.go
+++ memory/journal.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 )
 
-// Journal ...
+// Journal is a simple memory-based implementation of karigo.Journal.
+//
+// It is meant to be used for testing and examples. It is not meant to be used
+// in production.
 type Journal struct {
 	log   [][]byte
 	start uint
 }
 
-// Append ...
+// Append appends c to the log.
 func (j *Journal) Append(c []byte) error {
 	j.check()
 	if c == nil {
@@ -21,7 +24,7 @@ func (j *Journal) Append(c []byte) error {
 	return nil
 }
 
-// Last ...
+// Last returns the newest entry.
 func (j *Journal) Last() (uint, []byte, error) {
 	j.check()
 	if len(j.log) > 0 {
@@ -31,7 +34,7 @@ func (j *Journal) Last() (uint, []byte, error) {
 	return 0, nil, errors.New("karigo: journal is empty")
 }
 
-// At ...
+// At returns the entry indexed at i.
 func (j *Journal) At(i uint) ([]byte, error) {
 	j.check()
 	if i < j.start || i > j.start+uint(len(j.log))-1 {
@@ -40,14 +43,26 @@ func (j *Journal) At(i uint) ([]byte, error) {
 	return j.log[i-j.start], nil
 }
 
-// Cut ...
+// Cut removes all entries from the oldest one to the one at index i.
+//
+// It returns an error if i is greater than the newest index.
 func (j *Journal) Cut(i uint) error {
 	j.check()
-	// TODO
+	if i > j.start+uint(len(j.log)) {
+
+	} else if i < j.start {
+		// Already cut
+		return nil
+	} else {
+
+	}
 	return nil
 }
 
-// Range ...
+// Range returns a slice of entries from indexes n to s (inclusively).
+//
+// It returns an error if it can't return the ranger, whether it is because the
+// journal's history starts after n or s is greater than the newest index.
 func (j *Journal) Range(n uint, s uint) ([][]byte, error) {
 	j.check()
 	// TODO
