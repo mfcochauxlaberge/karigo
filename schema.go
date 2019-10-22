1c80a4056b1047210e96703d73284e72e03549f0
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-10-22 00:16:24 -0400

    Save

--- schema.go
+++ schema.go
@@ -1,7 +1,6 @@
 package karigo
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/mfcochauxlaberge/jsonapi"
@@ -142,99 +141,99 @@ type op struct {
 }
 
 func handleSchemaChange(s *jsonapi.Schema, r *Request, cp *Checkpoint) {
-	var (
-		res jsonapi.Resource
-		ops []Op
-		err error
-	)
+	// var (
+	// 	res jsonapi.Resource
+	// 	ops []Op
+	// 	err error
+	// )
 
-	res, _ = r.Doc.Data.(jsonapi.Resource)
+	// res, _ = r.Doc.Data.(jsonapi.Resource)
 
-	err = validateSchemaChange(res)
-	if err != nil {
-		cp.Check(err)
-	}
+	// err = validateSchemaChange(res)
+	// if err != nil {
+	// 	cp.Check(err)
+	// }
 
-	if r.Method == "POST" {
-		if res.GetType().Name == "0_sets" {
-			res.SetID(res.Get("name").(string))
+	// if r.Method == "POST" {
+	// 	if res.GetType().Name == "0_sets" {
+	// 		res.SetID(res.Get("name").(string))
 
-			// Add set
-			ops, err = addSet(s, res)
-			cp.Check(err)
-			cp.Apply(ops)
-		} else if res.GetType().Name == "0_attrs" {
-			res.SetID(res.GetToOne("set") + "_" + res.Get("name").(string))
+	// 		// Add set
+	// 		ops, err = addSet(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	} else if res.GetType().Name == "0_attrs" {
+	// 		res.SetID(res.GetToOne("set") + "_" + res.Get("name").(string))
 
-			// Add attribute
-			ops, err = addAttr(s, res)
-			cp.Check(err)
-			cp.Apply(ops)
-		} else if res.GetType().Name == "0_rels" {
-			_ = jsonapi.Rel{
-				FromType: res.GetToOne("from-set"),
-				FromName: "",
-				ToOne:    false,
-				ToType:   res.GetToOne("to-set"),
-				ToName:   "",
-				FromOne:  false,
-			}
-			res.SetID(res.GetToOne("from-set") + "_" + res.Get("name").(string))
-			res.SetID(res.GetToOne("to-set") + "_" + res.Get("name").(string))
+	// 		// Add attribute
+	// 		ops, err = addAttr(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	} else if res.GetType().Name == "0_rels" {
+	// 		_ = jsonapi.Rel{
+	// 			FromType: res.GetToOne("from-set"),
+	// 			FromName: "",
+	// 			ToOne:    false,
+	// 			ToType:   res.GetToOne("to-set"),
+	// 			ToName:   "",
+	// 			FromOne:  false,
+	// 		}
+	// 		res.SetID(res.GetToOne("from-set") + "_" + res.Get("name").(string))
+	// 		res.SetID(res.GetToOne("to-set") + "_" + res.Get("name").(string))
 
-			// Add relationship
-			ops, err = addRel(s, res)
-			cp.Check(err)
-			cp.Apply(ops)
-		}
-	} else if r.Method == "PATCH" {
-		// Can only be for activating or deactivating
-		// a set, attribute, or relationship.
-		if activate, ok := res.Get("active").(bool); activate && ok {
-			switch r.URL.ResType {
-			case "0_sets":
-				ops, err = activateSet(s, res)
-			case "0_attributes":
-				ops, err = activateAttr(s, res)
-			case "0_relationships":
-				ops, err = activateRel(s, res)
-			}
-			cp.Check(err)
-			cp.Apply(ops)
-		}
+	// 		// Add relationship
+	// 		ops, err = addRel(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	}
+	// } else if r.Method == "PATCH" {
+	// 	// Can only be for activating or deactivating
+	// 	// a set, attribute, or relationship.
+	// 	if activate, ok := res.Get("active").(bool); activate && ok {
+	// 		switch r.URL.ResType {
+	// 		case "0_sets":
+	// 			ops, err = activateSet(s, res)
+	// 		case "0_attributes":
+	// 			ops, err = activateAttr(s, res)
+	// 		case "0_relationships":
+	// 			ops, err = activateRel(s, res)
+	// 		}
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	}
 
-		if deactivate, ok := res.Get("active").(bool); !deactivate && ok {
-			switch r.URL.ResType {
-			case "0_sets":
-				ops, err = deactivateSet(s, res)
-			case "0_attributes":
-				ops, err = deactivateAttr(s, res)
-			case "0_relationships":
-				ops, err = deactivateRel(s, res)
-			}
-			cp.Check(err)
-			cp.Apply(ops)
-		}
-	} else if r.Method == "DELETE" {
-		currRes := cp.Resource(QueryRes{
-			Set:    res.GetType().Name,
-			ID:     res.GetID(),
-			Fields: []string{"active"},
-		})
+	// 	if deactivate, ok := res.Get("active").(bool); !deactivate && ok {
+	// 		switch r.URL.ResType {
+	// 		case "0_sets":
+	// 			ops, err = deactivateSet(s, res)
+	// 		case "0_attributes":
+	// 			ops, err = deactivateAttr(s, res)
+	// 		case "0_relationships":
+	// 			ops, err = deactivateRel(s, res)
+	// 		}
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	}
+	// } else if r.Method == "DELETE" {
+	// 	currRes := cp.Resource(QueryRes{
+	// 		Set:    res.GetType().Name,
+	// 		ID:     res.GetID(),
+	// 		Fields: []string{"active"},
+	// 	})
 
-		if currRes.GetID() == "" {
-			cp.Fail(errors.New("schema element does not exist"))
-		}
+	// 	if currRes.GetID() == "" {
+	// 		cp.Fail(errors.New("schema element does not exist"))
+	// 	}
 
-		if active, _ := currRes.Get("active").(bool); !active {
-			// Only possible is active is false
-			ops, err = deleteSet(s, res)
-			cp.Check(err)
-			cp.Apply(ops)
-		} else {
-			cp.Fail(errors.New("schema element is still active"))
-		}
-	}
+	// 	if active, _ := currRes.Get("active").(bool); !active {
+	// 		// Only possible is active is false
+	// 		ops, err = deleteSet(s, res)
+	// 		cp.Check(err)
+	// 		cp.Apply(ops)
+	// 	} else {
+	// 		cp.Fail(errors.New("schema element is still active"))
+	// 	}
+	// }
 }
 
 func validateSchemaChange(res jsonapi.Resource) error {
