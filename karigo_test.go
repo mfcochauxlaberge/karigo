569efae198677243409a4d5702ada76b724a1ef6
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-11-17 18:02:18 -0500

    Save

--- karigo_test.go
+++ karigo_test.go
@@ -3,7 +3,9 @@ package karigo_test
 import (
 	"bytes"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"strconv"
 	"testing"
 	"time"
 
@@ -26,17 +28,20 @@ func TestKarigo(t *testing.T) {
 	node := karigo.NewNode(&memory.Journal{}, src)
 	node.Name = "test"
 	node.Domains = []string{"localhost", "127.0.0.1"}
+
 	for _, domain := range node.Domains {
 		server.Nodes[domain] = node
 	}
 
+	port := findFreePort()
+
 	go func() {
-		server.Run()
+		server.Run(port)
 	}()
 	time.Sleep(100 * time.Millisecond)
 
 	// Test
-	res, err := post("/0_meta", []byte(`
+	res, err := post(port, "/0_meta", []byte(`
 		{
 			"data": {
 				"attributes": {
@@ -67,20 +72,23 @@ func TestKarigo(t *testing.T) {
 				"self": "/0_meta?fields%5B0_meta%5D=value&page%5Bsize%5D=10&sort=value%2Cid"
 			}
 		}`
+
 	assert.NoError(err)
-	assert.Equal(expect, res)
+	assert.JSONEq(expect, string(res))
 }
 
-func post(url string, body []byte) ([]byte, error) {
+func post(port uint, url string, body []byte) ([]byte, error) {
 	client := &http.Client{}
+
 	res, err := client.Post(
-		"http://localhost:8080"+url,
+		"http://localhost:"+strconv.Itoa(int(port))+url,
 		"application/vnd.api+json",
 		bytes.NewBuffer(body),
 	)
 	if err != nil {
 		return nil, err
 	}
+	defer res.Body.Close()
 
 	resBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
@@ -88,4 +96,19 @@ func post(url string, body []byte) ([]byte, error) {
 	}
 
 	return resBody, err
+}
+
+func findFreePort() uint {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0
+	}
+	defer l.Close()
+
+	return uint(l.Addr().(*net.TCPAddr).Port)
 }
