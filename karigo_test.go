dd863d0e61c107682dc1003ecc65a85cf822197b
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-11-30 17:05:17 -0500

    Save

--- karigo_test.go
+++ karigo_test.go
@@ -4,11 +4,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
+	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 	"testing"
@@ -22,39 +23,122 @@ import (
 
 var update = flag.Bool("update-golden-files", false, "update the golden files")
 
+func TestMain(m *testing.M) {
+	if *update {
+		err := os.RemoveAll("testdata")
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.Mkdir(filepath.Join("testdata"), os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.Mkdir(filepath.Join("testdata", "goldenfiles"), os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.Mkdir(filepath.Join("testdata", "goldenfiles", "replays"), os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	os.Exit(m.Run())
+}
+
 func TestKarigo(t *testing.T) {
 	assert := assert.New(t)
 
 	host := startServer()
 
 	tests := []struct {
-		name    string
-		method  string
-		path    string
-		payload string
+		name     string
+		requests []request
 	}{
 		{
-			name:   "example",
-			method: "POST",
-			path:   "/0_meta",
-			payload: `
+			name: "meta",
+			requests: []request{
 				{
-					"data": {
-						"attributes": {
-							"value": "some value"
-						},
-						"id": "some-key",
-						"type": "0_meta"
-					}
-				}
-			`,
+					method: "POST",
+					path:   "/0_meta",
+					payload: `
+						{
+							"data": {
+								"attributes": {
+									"value": "some value"
+								},
+								"id": "some-key",
+								"type": "0_meta"
+							}
+						}
+					`,
+				}, {
+					method: "GET",
+					path:   "/0_meta/some-key",
+				}, {
+					method: "PATCH",
+					path:   "/0_meta/some-key",
+					payload: `
+						{
+							"data": {
+								"attributes": {
+									"value": "value changed"
+								},
+								"id": "some-key",
+								"type": "0_meta"
+							}
+						}
+					`,
+				}, {
+					method: "GET",
+					path:   "/0_meta/some-key",
+				}, {
+					method: "DELETE",
+					path:   "/0_meta/some-key",
+				}, {
+					method: "GET",
+					path:   "/0_meta/some-key",
+				},
+			},
 		},
 	}
 
 	for _, test := range tests {
-		payload, headers, err := do(test.method, host, test.path, []byte(test.payload))
-		fmt.Printf("err: %v\n", err)
-		fmt.Printf("payload: %v\n", payload)
+		out := []byte{}
+
+		for i, req := range test.requests {
+			status, headers, body, err := do(req.method, host, req.path, []byte(req.payload))
+			assert.NoError(err)
+
+			out = append(out, req.method...)
+			out = append(out, ' ')
+			out = append(out, req.path...)
+			out = append(out, '\n')
+			out = append(out, '\n')
+
+			out = append(out, []byte(strconv.Itoa(status))...)
+			out = append(out, ' ')
+			out = append(out, []byte(http.StatusText(status))...)
+			out = append(out, '\n')
+
+			out = append(out, headers...)
+			out = append(out, '\n')
+
+			if len(body) > 0 {
+				out = append(out, body...)
+				out = append(out, '\n')
+				out = append(out, '\n')
+			}
+
+			if i < len(test.requests)-1 {
+				out = append(out, []byte("##################################################")...)
+				out = append(out, '\n')
+				out = append(out, '\n')
+			}
+		}
 
 		// Golden file
 		filename := strings.Replace(test.name, " ", "_", -1)
@@ -62,24 +146,16 @@ func TestKarigo(t *testing.T) {
 
 		if !*update {
 			// Retrieve the expected result from a file
-			expected, _ := ioutil.ReadFile(path)
-
+			expected, err := ioutil.ReadFile(path)
 			assert.NoError(err, test.name)
-			assert.JSONEq(string(expected), string(payload))
+			assert.Equal(string(expected), string(out), test.name)
 		} else {
 			// Write the result to a file
-			dst := &bytes.Buffer{}
-			err = json.Indent(dst, payload, "", "\t")
-			assert.NoError(err)
-			out := headers
-			out = append(out, '\n')
-			out = append(out, dst.Bytes()...)
 			// TODO Figure out whether 0644 is okay or not.
-			err = ioutil.WriteFile(path, out, 0644)
+			err := ioutil.WriteFile(path, out, 0644)
 			assert.NoError(err)
 		}
 	}
-
 }
 
 func startServer() string {
@@ -108,54 +184,70 @@ func startServer() string {
 	return "127.0.0.1:" + strconv.Itoa(int(port))
 }
 
-func do(method, host, path string, body []byte) ([]byte, []byte, error) {
-	fmt.Printf("url: %s\n", host+path)
-
+func do(method, host, path string, body []byte) (int, []byte, []byte, error) {
+	// Build request
 	req, err := http.NewRequest(
 		method,
 		"http://"+host+path,
 		bytes.NewBuffer(body),
 	)
 	if err != nil {
-		return nil, nil, err
+		return 0, nil, nil, err
 	}
 
+	// Send request
 	client := &http.Client{}
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("errrrrr: %v\n", err)
-		return nil, nil, err
+		return 0, nil, nil, err
 	}
 	defer res.Body.Close()
 
+	// Read response
 	resBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, nil, err
+		return 0, nil, nil, err
 	}
 
-	header := []byte{}
+	// Format response body
+	if len(resBody) > 0 {
+		dst := &bytes.Buffer{}
+		err = json.Indent(dst, resBody, "", "\t")
+		resBody = dst.Bytes()
+	}
 
-	for key, vals := range res.Header {
-		if len(header) > 0 {
-			header = append(header, '\n')
-		}
+	return res.StatusCode, buildSortedHeader(res.Header), resBody, err
+}
 
-		header = append(header, []byte(key)...)
-		header = append(header, []byte(": ")...)
+func buildSortedHeader(h http.Header) []byte {
+	header := []string{}
+
+	for key, vals := range h {
+		header = append(header, "")
+		j := len(header) - 1
+
+		header[j] = key + ": "
 
 		if key == "Date" {
-			header = append(header, []byte("Abc, 01 Def 2345 67:89:01 GMT")...)
+			header[j] += "Abc, 01 Def 2345 67:89:01 GMT"
 		} else {
 			for i := 0; i < len(vals); i++ {
-				header = append(header, []byte(vals[i])...)
+				header[j] += vals[i]
 			}
 		}
+
+		header[j] += "\n"
 	}
 
-	header = append(header, header...)
+	sort.Strings(header)
 
-	return resBody, header, err
+	headerBytes := []byte{}
+	for _, v := range header {
+		headerBytes = append(headerBytes, []byte(v)...)
+	}
+
+	return headerBytes
 }
 
 func findFreePort() uint {
@@ -171,4 +263,10 @@ func findFreePort() uint {
 	defer l.Close()
 
 	return uint(l.Addr().(*net.TCPAddr).Port)
+}
+
+type request struct {
+	method  string
+	path    string
+	payload string
 }
