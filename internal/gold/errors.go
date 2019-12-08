bde128df1de7bd20bff785b7e5d51dca28edaf2e
Marc-François Cochaux-Laberge <dev@mfcl.io>
2019-12-08 16:32:18 -0500

    Add internal/gold for handling golden files

---
+++ internal/gold/errors.go
@@ -0,0 +1,10 @@
+package gold
+
+// ComparisonError represents a difference between a given output and the
+// content of a file.
+type ComparisonError struct {
+}
+
+func (e ComparisonError) Error() string {
+	return "output and file are different"
+}
