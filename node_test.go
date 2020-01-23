c43d43528a5ef96c0e16f40bab71a71bbf1c39d5
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-01-22 22:07:59 -0500

    Reorder imports in node_test.go

--- node_test.go
+++ node_test.go
@@ -3,10 +3,10 @@ package karigo_test
 import (
 	"testing"
 
-	"github.com/mfcochauxlaberge/jsonapi"
-
 	. "github.com/mfcochauxlaberge/karigo"
 	"github.com/mfcochauxlaberge/karigo/memory"
+
+	"github.com/mfcochauxlaberge/jsonapi"
 )
 
 func TestNode(t *testing.T) {
