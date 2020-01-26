1db5d017417be5737962c376c6f2aa3fdaa35033
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-01-26 16:53:35 -0500

    Save

--- memory/source_test.go
+++ memory/source_test.go
@@ -8,8 +8,9 @@ import (
 
 func TestMemorySource(t *testing.T) {
 	src := &Source{}
+	jrnl := &Journal{}
 
-	err := sourcetest.Test(t, src)
+	err := sourcetest.Test(t, src, jrnl)
 	if err != nil {
 		t.Errorf("Source %T failed: %s", src, err)
 	}
