569d8ebb7607155ebae8bf97f256dca3f5339cca
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-02-16 09:54:33 -0500

    Add Config struct

--- cmd/karigo/commands/run.go
+++ cmd/karigo/commands/run.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"github.com/mfcochauxlaberge/karigo"
 	"github.com/mfcochauxlaberge/karigo/cmd/karigo/util"
 
 	"github.com/spf13/cobra"
@@ -10,15 +11,19 @@ var cmdRun = &cobra.Command{
 	Use:   "run",
 	Short: "Run the server",
 	Run: func(cmd *cobra.Command, args []string) {
-		server := util.CreateServer()
-		server.Run(*port)
+		server := util.CreateServer(karigo.Config{
+			Port: 6280,
+			Host: "127.0.0.1",
+			Journal: map[string]string{
+				"type": "memory",
+			},
+			Sources: map[string]map[string]string{
+				"main": {
+					"type": "memory",
+				},
+			},
+		})
+
+		server.Run()
 	},
-}
-
-var (
-	port *uint
-)
-
-func init() {
-	port = cmdRun.Flags().UintP("port", "p", 6280, "port number")
 }
