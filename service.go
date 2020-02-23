ddfef1faac5a5ec537d93cb48af27c34a49d8ef2
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-02-23 01:57:23 -0500

    Add top comment for Service interface

--- service.go
+++ service.go
@@ -1,6 +1,11 @@
 package karigo
 
-// Service ...
+// A Service is the interface to handle external services, whether they are on
+// the same host or another one.
+//
+// This is used for managing journals and sources. If an implementation does not
+// represent an external service, the methods can pretend every is fine. For
+// example, Connect could always return nil.
 type Service interface {
 	// Connect instantiates a connection to the service behind
 	// the source.
