1b67cb28b6312061cce7e9cccf60c7d72f193d8b
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-03-15 11:30:21 -0400

    Improve v0 op encoding/decoding

--- op_encode.go
+++ op_encode.go
@@ -4,10 +4,17 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
+
+	"github.com/mfcochauxlaberge/jsonapi"
 )
 
 // Encode ...
 func Encode(v uint, ops []Op) ([]byte, error) {
+	if len(ops) == 0 {
+		return nil, errors.New("no ops")
+	}
+
 	var (
 		enc []byte
 		err error
@@ -16,9 +23,9 @@ func Encode(v uint, ops []Op) ([]byte, error) {
 	switch v {
 	case 0:
 		// Version 0
-		enc, err = json.Marshal(ops)
+		enc, err = encodeV0(ops)
 		if err != nil {
-			return nil, fmt.Errorf("cannot encode: %s", err)
+			return nil, err
 		}
 	default:
 		return nil, errors.New("unsupported version")
@@ -28,22 +35,127 @@ func Encode(v uint, ops []Op) ([]byte, error) {
 }
 
 // Decode ...
-func Decode(v uint, raw []byte) ([]Op, error) {
-	if len(raw) == 0 {
+func Decode(v uint, data []byte) ([]Op, error) {
+	if len(data) == 0 {
 		return nil, errors.New("no bytes")
 	}
 
-	ops := []Op{}
+	var (
+		ops []Op
+		err error
+	)
 
 	switch v {
 	case 0:
 		// Version 0
-		err := json.Unmarshal(raw, &ops)
+		ops, err = decodeV0(data)
 		if err != nil {
-			return nil, fmt.Errorf("cannot decode: %s", err)
+			return nil, err
 		}
 	default:
 		return nil, errors.New("unsupported version")
+	}
+
+	return ops, nil
+}
+
+func encodeV0(ops []Op) ([]byte, error) {
+	enc := make([]json.RawMessage, 0)
+
+	for _, op := range ops {
+		m := map[string]interface{}{}
+
+		key, err := json.Marshal(map[string]string{
+			"set":   op.Key.Set,
+			"id":    op.Key.ID,
+			"field": op.Key.Field,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		m["key"] = json.RawMessage(key)
+		m["op"] = string(op.Op)
+		m["value"] = op.Value
+
+		if op.Value == nil {
+			return nil, errors.New("op has nil value")
+		}
+
+		m["type"] = jsonapi.GetAttrTypeString(
+			jsonapi.GetAttrType(fmt.Sprintf("%T", op.Value)),
+		)
+
+		data, err := json.Marshal(m)
+		if err != nil {
+			return nil, err
+		}
+
+		enc = append(enc, data)
+	}
+
+	res, err := json.Marshal(enc)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func decodeV0(data []byte) ([]Op, error) {
+	ops := []Op{}
+
+	raws := []json.RawMessage{}
+
+	err := json.Unmarshal(data, &raws)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, raw := range raws {
+		op := Op{}
+		m := map[string]json.RawMessage{}
+
+		err = json.Unmarshal(raw, &m)
+		if err != nil {
+			return nil, err
+		}
+
+		// Key
+		err = json.Unmarshal(m["key"], &op.Key)
+		if err != nil {
+			return nil, err
+		}
+
+		// Op
+		opStr := ""
+
+		err := json.Unmarshal(m["op"], &opStr)
+		if err != nil {
+			return nil, err
+		}
+
+		op.Op = NewOp(opStr)
+		if op.Op == 0 {
+			return nil, fmt.Errorf("unknown op %q", op.Op)
+		}
+
+		// Value
+		typ := strings.Trim(string(m["type"]), `"`)
+
+		t, n := jsonapi.GetAttrType(typ)
+		attr := jsonapi.Attr{
+			Name:     "",
+			Type:     t,
+			Nullable: n,
+		}
+
+		op.Value, err = attr.UnmarshalToType(m["value"])
+		if err != nil {
+			return nil, err
+		}
+
+		ops = append(ops, op)
 	}
 
 	return ops, nil
