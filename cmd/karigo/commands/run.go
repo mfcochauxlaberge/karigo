39a0d9a1a7df74673cff05317e7dcf187c5d7401
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-03-16 22:30:08 -0400

    Read configuration from file

--- cmd/karigo/commands/run.go
+++ cmd/karigo/commands/run.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"github.com/mfcochauxlaberge/karigo"
 	"github.com/mfcochauxlaberge/karigo/cmd/karigo/util"
 
 	"github.com/spf13/cobra"
@@ -11,18 +10,7 @@ var cmdRun = &cobra.Command{
 	Use:   "run",
 	Short: "Run the server",
 	Run: func(cmd *cobra.Command, args []string) {
-		server := util.CreateServer(karigo.Config{
-			Port:  6280,
-			Hosts: []string{"127.0.0.1"},
-			Journal: map[string]string{
-				"type": "memory",
-			},
-			Sources: map[string]map[string]string{
-				"main": {
-					"type": "memory",
-				},
-			},
-		})
+		server := util.CreateServer(Config)
 
 		server.Run()
 	},
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/shopspring/decimal v0.0.0-20200227202807-02e2044944cc // indirect
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.5.1
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
