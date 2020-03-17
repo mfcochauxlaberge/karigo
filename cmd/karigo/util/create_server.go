71c039422ff8624db5a054d08a869102efadb289
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-03-16 22:20:34 -0400

    Use config to set up server in CreateServer

--- cmd/karigo/util/create_server.go
+++ cmd/karigo/util/create_server.go
@@ -3,20 +3,37 @@ package util
 import (
 	"github.com/mfcochauxlaberge/karigo"
 	"github.com/mfcochauxlaberge/karigo/drivers/memory"
+	"github.com/mfcochauxlaberge/karigo/drivers/psql"
 )
 
 func CreateServer(config karigo.Config) *karigo.Server {
 	// Server
 	server := karigo.NewServer(config)
 
+	// Journal
+	var jrnl karigo.Journal
+
+	switch config.Journal["driver"] {
+	case "memory":
+		jrnl = &memory.Journal{}
+	case "psql":
+		jrnl = &psql.Journal{}
+	default:
+		jrnl = &memory.Journal{}
+	}
+
+	// Source
+	// TODO Stop ignoring this configuration and
+	// use the proper driver for the source.
 	src := &memory.Source{}
+
 	_ = src.Reset()
 
 	// Add cluster control node
-	ctlNode := karigo.NewNode(&memory.Journal{}, src)
+	ctlNode := karigo.NewNode(jrnl, src)
 	ctlNode.Name = "main_node"
 
-	for _, host := range server.Hosts {
+	for _, host := range config.Hosts {
 		server.Nodes[host] = ctlNode
 	}
 
