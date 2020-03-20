ab77a31642873316b8d5216680f4246b3013f169
Marc-François Cochaux-Laberge <dev@mfcl.io>
2020-03-19 22:37:52 -0400

    Remove Source instantiation from CreateServer

--- cmd/karigo/util/create_server.go
+++ cmd/karigo/util/create_server.go
@@ -2,20 +2,12 @@ package util
 
 import (
 	"github.com/mfcochauxlaberge/karigo"
-	"github.com/mfcochauxlaberge/karigo/drivers/memory"
 )
 
 func CreateServer(config karigo.Config) *karigo.Server {
 	// Server
 	server := karigo.NewServer()
 	server.Config = config
-
-	// Source
-	// TODO Stop ignoring this configuration and
-	// use the proper driver for the source.
-	src := &memory.Source{}
-
-	_ = src.Reset(karigo.FirstSchema())
 
 	// Add cluster control node
 	ctlNode := karigo.NewNode(config)
